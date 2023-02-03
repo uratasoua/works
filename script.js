08d1979a98649e0181ddefe000a22444c2f052f6
uratasoua <110446626+uratasoua@users.noreply.github.com>
2023-02-03 13:39:28 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$('.slider__list').slick({
+  autoplay: true,         //自動再生
+  autoplaySpeed: 2000,    //自動再生のスピード
+  fade: true,             //再生の仕方をフェードイン・フェードアウトにする
+  speed: 2200,             //スライドするスピード
+  dots: true,             //スライドしたのドット
+  arrows: true,           //左右の矢印
+  infinite: true,         //スライドのループ
+  pauseOnHover: false,    //ホバーしたときにスライドを一時停止しない　
+});
